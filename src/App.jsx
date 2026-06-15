e340c3efcac0631ced86283cd4b1e7e331ad7402
himarbi <engibrahimabukarahmed@gmail.com>
2026-06-15 21:12:00 +0300

    Configure GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
